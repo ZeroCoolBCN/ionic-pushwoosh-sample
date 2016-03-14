c9609d6423d0f08debf3462deef4fa9d8992bb85
leif <leif@beansmile.com>
2016-03-14 22:12:06 +0800

    ionic pushwoosh exmaple

---
+++ www/js/app.config.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular.module('ionicPushwooshSample.configs', [])
+
+  .constant("PUSHWOOSH_CONFIG", {
+    "PW_APP_ID": '28619-D5090',
+    "GOOGLE_PROJECT_NUMBER": '915816252356'
+  })
+})();
+
